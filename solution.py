a46c31acf96fa3e7d6eae4ec926a17a4c5a23bad
johnnymadigan <silkjay@icloud.com>
2021-05-20 17:45:16 +1000

    Continued shortening code
    List comprehension, short-hands etc.

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@
 #░░░██║░░░███████╗██████╔╝░░░██║░░░  ██████╔╝██║░░██║░░░██║░░░██║░░██║
 #░░░╚═╝░░░╚══════╝╚═════╝░░░░╚═╝░░░  ╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝
 
-#testData = ["GRW", "GRY", "GOY", "GOW", "BOW", "BOY", "BRY", "zrw"] # expecting warning msg (no such corner ZRW)
+#testData = ["GRW", "GRY", "GOY", "GOW", "BOW", "BOY", "BRY", "zzz"] # expecting warning msg (no such corner ZRW)
 #testData = ["GRW", "GRY", "GOY", "GOW", "BOW", "BOY", "BRY", "BRW"] # expecting 0 steps (cube already solved)
 #testData = ["GRY", "BRY", "GOY", "GOW", "BOW", "BOY", "BRW", "GRW"] # expecting 1 steps (applied 1 legal move)
 #testData = ["GRY", "BRY", "OGY", "OGW", "OBW", "OBY", "BRW", "GRW"] # expecting 1 steps (same as above except not in alphabetical order)
@@ -44,7 +44,7 @@ def NS_out(V, E, S):
 #╚═════╝░░╚════╝░╚══════╝░╚═════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
 
 def solution(problemCube):    
-    solvedCube = "GRWGRYGOYGOWBOWBOYBRYBRW" # starting cube (each corner's colours are in alphabetical order)
+    solvedCube = "GRWGRYGOYGOWBOWBOYBRYBRW" # starting cube & starting vertex for distance classes
 
     legalCorners = ["GRW", "GRY", "GOY", "GOW", "BOW", "BOY", "BRY", "BRW"]
     legalMoves = [
@@ -55,25 +55,18 @@ def solution(problemCube):
         [0,6,1,3,4,2,5,7],  # R  = right clockwise
         [0,2,5,3,4,6,1,7]]  # R' = right anti-clockwise
 
-    afterSorted = list()
-    for corner in problemCube:
-        sortedCorner = ("".join(sorted(corner.upper())))
-        for legalCorner in legalCorners:
-            if legalCorner == sortedCorner: afterSorted.append(sortedCorner)#; print(sorted(corner.upper())); print(legalCorner)
+    # sort the problem cube's corner's colours in alphabetical order & make them all uppercase for consistency
+    # when sorting, we will only save the corners that are a member of the 8 legal corners (regardless of position)
+    afterSorted = [[("".join(sorted(corner.upper()))) for legalCorner in legalCorners if legalCorner == ("".join(sorted(corner.upper())))]for corner in problemCube]
+    afterSorted = [item for sublist in afterSorted for item in sublist] # flatten to remove nested lists
 
-    #afterSorted = list()
-    #splitProblemCube = [problemCube.upper()[i:i+3] for i in range(0, len(problemCube), 3)] # split string into groups of 3 (corners)
-    #for corner in splitProblemCube:
-    #    for legalCorner in legalCorners:
-    #        if ("".join(sorted(corner))) == legalCorner: afterSorted.append("".join(sorted(corner)))
-
+    # if not all 8 legal corners are found, the problem cube instance is invalid
     if len(afterSorted) < 8: 
         print("Invalid problem cube, please make sure you use the right corners in your string")
         return [{}]
 
+    # get a list of the same problem cube but from different POVs (when looking at the 8 corners) keeps them in sequence which is crucial
     sortedProblemCube = "".join(afterSorted)
-    
-    # get the same problem cube but from different POVs (when looking at the 8 corners) keeps them in sequence which is crucial
     problemCubes = [sortedProblemCube[-3*view:] + sortedProblemCube[:-3*view] for view in range(6)]
 
     found = False           # store the found problem cube (any 1 out of 8 of the POVs) also a flag
@@ -84,25 +77,31 @@ def solution(problemCube):
 
     while not found:
         for cube in problemCubes:
-            if cube in V: 
-                distances = distanceClasses(V, E, solvedCube) # get the set of distances
-                distances.insert(len(distances),cube) # save the problem cube in the data structure, will be popped in printSolution()
+            # if problem cube is found, get the set of distance classes
+            # save the problem cube in the distances data structure, will be popped in printSolution()
+            if cube in V:
+                distances = distanceClasses(V, E, solvedCube)
+                distances.insert(len(distances),cube)
                 return(distances)
 
-        print("searching...")  # let the user know the program is running as it can take some time...
+        print("searching...")  # let the user know the program is running
         for cube in toRotate:
-            for move in range(6): # for 6 legal moves
-                splitCorners = [cube[i:i+3] for i in range(0, len(cube), 3)] # ensures corners are moved in groups of 3 (also makes legalMoves 8x smaller)
-                newCube = ''.join([splitCorners[i] for i in legalMoves[move]]) # apply move & create new permitation
-                
-                temporary.add(newCube) # temporarily save new permitation to be rotated next time
-                V.add(newCube) # save new permitation as a vertex (does not add duplicates)
-                E.add((newCube, cube)) # save relationship edge
-                E.add((cube, newCube)) # save reverse edge as graph is un-directed (also to avoid maximum recursion depth error)
+            # for the 6 legal moves, ensure the corners are moved in groups of threes (also makes legalMoves[] 8x smaller)
+            # apply a legal move to create the new permitation
+            for move in range(6): 
+                splitCorners = [cube[i:i+3] for i in range(0, len(cube), 3)]
+                newCube = ''.join([splitCorners[i] for i in legalMoves[move]])
+                # add new vertex (cube), new edges (parent-child relationships), and save the cube temporarily to be rotated next time 
+                V.add(newCube)
+                E.add((newCube, cube))
+                E.add((cube, newCube))
+                temporary.add(newCube)
 
-        V = V.union(toRotate) # save all parent cubes as vertices
+        # reset
+        V = V.union(toRotate)
         toRotate = temporary
         temporary = set()
+
 
 
 #██████╗░██████╗░██╗███╗░░██╗████████╗  ░██████╗░█████╗░██╗░░░░░██╗░░░██╗████████╗██╗░█████╗░███╗░░██╗
@@ -117,7 +116,7 @@ def printSolution(distances):
     for d in distances:
         if problemCube in d:
             index = distances.index(d)
-            if index == 0:
+            if index == 0: 
                 print("Cube already solved!")
             else:
                 print("Minimum number of steps to solve your cube are {0} steps!".format(index))
@@ -130,9 +129,9 @@ printSolution(solution(testData))
 #                _.-""   |  ',                  unique in the way that each corner has a different combination
 #        (GRW)+:"        |    ',                of 3 colours (out of six). These colours are...
 #              |\        |      '.+(BRY)
-#              | \     (BOW)  _."|              (W) red
-#              |  '.   _.+.,."   |              (R) yellow
-#              |   _\-",-' ",    |              (Y) white
+#              | \     (BOW)  _."|              (W) white
+#              |  '.   _.+.,."   |              (R) red
+#              |   _\-",-' ",    |              (Y) yellow
 #              |_-" "+'      ",  |              (G) green
 #        (GOW)+{   (GRY)       ".|              (B) blue
 #               \    |          ,+(BOY)         (O) orange
