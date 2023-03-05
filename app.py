610931c084f2e203ff3cea0e766d85573d3b7eb7
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2023-03-05 21:19:48 +1000

    [SCRIPT] Cleaned up solution

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 
 
-#                        FULL NAME: JOHNNY MADIGAN
-#                   STUDENT NUMBER: N????????
-
-
+#                            A J-MAD CREATION
+#                         @johnnymadigan on GitHub
+#                    scroll to bottom for starting point
 
 #████████╗███████╗░██████╗████████╗  ██████╗░░█████╗░████████╗░█████╗░
 #╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝  ██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗
@@ -34,32 +33,40 @@
 #
 #OBBBWWRORRBYYYOGOGWGWGYR               6 steps                              NEW testData from Assignment PDF Section 6
 
-testData = "OBBBWWRORRBYYYOGOGWGWGYR" # currently set to testData from Assignment PDF Section 6
+#██████╗░███████╗░██████╗
+#██╔══██╗██╔════╝██╔════╝
+#██████╦╝█████╗░░╚█████╗░
+#██╔══██╗██╔══╝░░░╚═══██╗
+#██████╦╝██║░░░░░██████╔╝
+#╚═════╝░╚═╝░░░░░╚═════╝░
 
-#██████╗░██╗░██████╗████████╗░█████╗░███╗░░██╗░█████╗░███████╗ ░█████╗░██╗░░░░░░█████╗░░██████╗░██████╗███████╗░██████╗
-#██╔══██╗██║██╔════╝╚══██╔══╝██╔══██╗████╗░██║██╔══██╗██╔════╝ ██╔══██╗██║░░░░░██╔══██╗██╔════╝██╔════╝██╔════╝██╔════╝
-#██║░░██║██║╚█████╗░░░░██║░░░███████║██╔██╗██║██║░░╚═╝█████╗░░ ██║░░╚═╝██║░░░░░███████║╚█████╗░╚█████╗░█████╗░░╚█████╗░
-#██║░░██║██║░╚═══██╗░░░██║░░░██╔══██║██║╚████║██║░░██╗██╔══╝░░ ██║░░██╗██║░░░░░██╔══██║░╚═══██╗░╚═══██╗██╔══╝░░░╚═══██╗
-#██████╔╝██║██████╔╝░░░██║░░░██║░░██║██║░╚███║╚█████╔╝███████╗ ╚█████╔╝███████╗██║░░██║██████╔╝██████╔╝███████╗██████╔╝
-#╚═════╝░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░╚══════╝ ░╚════╝░╚══════╝╚═╝░░╚═╝╚═════╝░╚═════╝░╚══════╝╚═════╝░
+# Since there are ~3.6 million (3,674,160) possible permutations, performance is crucial.
+# Best performing BFS algorithm from https://stackoverflow.com/a/50575971
+# Non-recursive since recursion ran slower. The following still keeps track of visited vertices.
+def BFS(graph, start, end):
+    """
+    BFS algorithm to find the shortest path from the solved cube to the instance.
 
-# From graphys.py obtained from CAB203 Blackboard
-def distanceClasses(V, E, u):
-    V0 = V              # V_0 = V
-    D = [ {u} ]         # D[0] = D_0 = {u}
-    print("Getting distance classes...")
-    return distanceClassesR(V0, E, D)
+    :param: graph: cube group where edges are cubes related via a quater turn
+    :param: start: solved cube
+    :param: end: instance cube
+    :returns: smallest list of vertices from the graph (cubes)
+    """
 
-# From graphys.py obtained from CAB203 Blackboard
-def distanceClassesR(V, E, D):
-    Vnew = V - D[-1]            # V_{j} = V_{j-1} / D_{j-1}
-    if len(Vnew) == 0: return D # Already considered all elements?
-    Dnew = D + [ NS_out(Vnew, E, D[-1]) ]  # D_{j} = N_{V_j}(D_{j-1})
-    return distanceClassesR(Vnew, E, Dnew)
+    queue = [(start,[start])]
+    visited = set()
 
-# From graphys.py obtained from CAB203 Blackboard
-def NS_out(V, E, S):
-    return { v for v in V for u in S if (u,v) in E }
+    print("Performing a Breadth-first search traversal...\n")
+
+    while queue:
+        vertex, path = queue.pop(0)
+        visited.add(vertex)
+
+        for node in graph[vertex]:
+            if node == end: return path + [end]
+            elif node not in visited:
+                    visited.add(node)
+                    queue.append((node, path + [node]))
 
 #░██████╗░█████╗░██╗░░░░░██╗░░░██╗████████╗██╗░█████╗░███╗░░██╗
 #██╔════╝██╔══██╗██║░░░░░██║░░░██║╚══██╔══╝██║██╔══██╗████╗░██║
@@ -68,9 +75,20 @@ def NS_out(V, E, S):
 #██████╔╝╚█████╔╝███████╗╚██████╔╝░░░██║░░░██║╚█████╔╝██║░╚███║
 #╚═════╝░░╚════╝░╚══════╝░╚═════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
 
-def solution(instance):    
-    solvedCube = "WWWWGGGGRRRRBBBBOOOOYYYY" # the solved instance
-    instance = instance.upper() # make sure instance is capitalised
+class IllegalCube(Exception): pass
+import time
+
+def solution(instance):
+    """
+    Generates a Cayley graph starting with the solved cube and stops at the instance. 
+    Calls a BFS to find the shortest path a.k.a the minimum steps to solve.
+
+    :param instance: the cube to solve
+    :returns: list of steps (cubes encoded as strings)
+    """
+
+    solvedCube = "WWWWGGGGRRRRBBBBOOOOYYYY"
+    instance = instance.upper() # Capitalise to make function case-insensitive
 
     # The 6 legal quarter turns rather than 12 as half the moves are redundent,
     # this is because we don't care about the orientation of the cube
@@ -81,7 +99,7 @@ def solution(instance):
         [0,9,2,11,4,5,6,7,8,21,10,23,14,12,15,13,3,17,1,19,20,18,22,16],            # R    right clockwise
         [0,18,2,16,4,5,6,7,8,1,10,3,13,15,12,14,23,17,21,19,20,9,22,11]]            # R'   right anti-clockwise
 
-    # Rotating the whole cube in 3D space (each face takes a turn being on top) 
+    # Rotating the whole cube in 3D space (each face takes a turn being on top)
     six = [[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
         [12,13,14,15,9,11,8,10,21,23,20,22,18,16,19,17,1,0,3,2,7,6,5,4],
         [8,9,10,11,5,7,4,6,20,21,22,23,14,12,15,13,3,2,1,0,19,18,17,16],
@@ -89,71 +107,125 @@ def solution(instance):
         [4,5,6,7,17,19,16,18,22,20,23,21,10,8,11,9,2,0,3,1,15,14,13,12],
         [16,17,18,19,13,15,12,14,23,22,21,20,6,4,7,5,0,1,2,3,11,10,9,8]]
 
-    # Spinning the whole cube keeping the same face down (4 x 90degree rotations)
+    # Spinning the whole cube keeping the same face down (turn 90 degrees clockwise four times)
     four = [[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
         [2,0,3,1,8,9,10,11,12,13,14,15,16,17,18,19,4,5,6,7,21,23,20,22],
         [3,2,1,0,12,13,14,15,16,17,18,19,4,5,6,7,8,9,10,11,23,22,21,20],
         [1,3,0,2,16,17,18,19,4,5,6,7,8,9,10,11,12,13,14,15,22,20,23,21]]
-
-    # Check early to see if the cube is scrambled properly,
-    # if so, return the correct data structure but empty, saving memory
-    if not sorted(solvedCube) == sorted(instance): 
-        print("Invalid scrambled cube, please make sure you have 4 of each colours")
-        return ({}, None) # return nothing
     
-    # Check early to see if the cube is already solved,
-    # if so, return the solved cube in the correct data structure, saving memory
-    if solvedCube == instance: return ([{solvedCube}], solvedCube)
+    # Check early to see if the instance is encoded properly, if not, throw exception
+    if not sorted(solvedCube) == sorted(instance): raise IllegalCube(instance)
+    
+    # Check early to see if the cube is already solved, if so, stop here to save memory
+    if solvedCube == instance: return [solvedCube]
 
-    # Since we want ultimate flexibility for user inputs, the instance may be in any orientation,
-    # therefore we must get all 24 orientations of the problem instance in 3D space so we can ignore the position
-    # THE ONLY TIME "SIX" & "FOUR" ARE USED
-    pCubes = set()
+    # To allow ultimate flexibility for user inputs, the instance may be in any orientation
+    # Therefore, store all 24 possible orientations of the instance so its position can be ignored
+    instancePositions = set()
     for rot in range(0,len(six)):
         newOrientation = ''.join([instance[i] for i in six[rot]])
-        [pCubes.add(''.join([newOrientation[i] for i in four[spin]])) for spin in range(0, len(four))]
+        [instancePositions.add(''.join([newOrientation[i] for i in four[spin]])) for spin in range(0, len(four))]
 
-    toRotate = {solvedCube} # Next set of parent cubes to rotate (starts off with the solved cube)
-    temporary = set()       # Temporary holding for next set of cubes to be rotated next after current rotations are done
-    E = set()               # Set of edges (cubes related by transforming to one-another with a quarter turn)
-    V = {solvedCube}        # Set of vertices (cubes)
+    graph = dict()          # Adjacency list (keys are vertices (cubes) and values are edges (cubes related by transforming to one-another with a quarter turn))
+    neighbours = list()     # Neighbour cubes for a parent cube, will be added to the graph (dictionary) with the parent cube as the key and the neighbours as the values
+    toRotate = {solvedCube} # Queue of parent cubes to rotate (starts off with the solved cube)
+    temporary = set()       # New neighbour cubes to be rotated next (as parent cubes)
 
-    print("\nsearching...\n")
-    while not pCubes.intersection(V): # while no orientations of the problem instance intersect the set of vertices
+    print("\nGenerating Pocket cube group graph...\n")
+
+    while not instancePositions.intersection(graph):
         
-        # For each parent cube we need to rotate,
-        # rotate the parent cube 6 times (each quarter move)
         for cube in toRotate:
             for move in range(0,len(legalMoves)):
-                newCube = ''.join([cube[i] for i in legalMoves[move]])  # Create a new instance rearranging the stickers to simulate a quarter move
-                temporary.add(newCube)                                  # Save the derived cube to be manipulated next round
-                V.add(newCube)                                          # Store the cube as a vertex
-                E.update(((newCube, cube),(cube, newCube)))             # Store edges both ways as this graph is undirected
+                # Generate neighbours
+                newCube = ''.join([cube[i] for i in legalMoves[move]])
+                neighbours.append(newCube)
                 
-            # If any orientation of the problem instances is found in the set of vertices, 
-            # generate and return the distance classes along with the specific orientation of the problem instance found
-            for c in pCubes:
-                if c in V:
-                    print("Found instance after {:,} permutations\n".format(len(V)))
-                    return ((distanceClasses(V, E, solvedCube)), c)
+            temporary.update(neighbours) # Store neighbours to be rotated next round
+            graph[cube] = neighbours     # Add neighbours to the graph
+            neighbours = list()          # Reset
 
-        # Transfer over the next set of vertices to generate new vertices from, and reset/empty the temporary set
-        toRotate = temporary
-        temporary = set()
-        print("Total permutations so far... {:,}".format(len(V))) 
+            # If any orientation of the instance is found in the graph, do a Breadth-first search
+            # to get a list of the cubes (vertices) that forms the shortest path.
+            # Return the list as it provides the minimum number of steps to solve the instance.
+            for instance in instancePositions:
+                if instance in graph:
+                    print("Found instance after {:,} permutations\n".format(len(graph)))
+                    shortestPath = (BFS(graph, solvedCube, instance))
+                    return shortestPath
+
+        toRotate = temporary # Transfer the new permutations to be rotated next
+        temporary = set()    # Reset for the next set of new permutations
 
 
-#██████╗░██████╗░██╗███╗░░██╗████████╗  ░██████╗░█████╗░██╗░░░░░██╗░░░██╗████████╗██╗░█████╗░███╗░░██╗
-#██╔══██╗██╔══██╗██║████╗░██║╚══██╔══╝  ██╔════╝██╔══██╗██║░░░░░██║░░░██║╚══██╔══╝██║██╔══██╗████╗░██║
-#██████╔╝██████╔╝██║██╔██╗██║░░░██║░░░  ╚█████╗░██║░░██║██║░░░░░██║░░░██║░░░██║░░░██║██║░░██║██╔██╗██║
-#██╔═══╝░██╔══██╗██║██║╚████║░░░██║░░░  ░╚═══██╗██║░░██║██║░░░░░██║░░░██║░░░██║░░░██║██║░░██║██║╚████║
-#██║░░░░░██║░░██║██║██║░╚███║░░░██║░░░  ██████╔╝╚█████╔╝███████╗╚██████╔╝░░░██║░░░██║╚█████╔╝██║░╚███║
-#╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░░░╚═╝░░░  ╚═════╝░░╚════╝░╚══════╝░╚═════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
+#██████╗░██████╗░██╗███╗░░██╗████████╗
+#██╔══██╗██╔══██╗██║████╗░██║╚══██╔══╝
+#██████╔╝██████╔╝██║██╔██╗██║░░░██║░░░
+#██╔═══╝░██╔══██╗██║██║╚████║░░░██║░░░
+#██║░░░░░██║░░██║██║██║░╚███║░░░██║░░░
+#╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░░░╚═╝░░░
 
-def printSolution(data):
-    [print("Minimum number of steps to solve your cube are {0} steps!\n".format(data[0].index(distance))) for distance in data[0] if data[1] in distance]
+def printSolution(steps):
+    """
+    Given the steps (solution), print the minimum number of steps to solve your cube.
 
-printSolution(solution(testData))
+    :param steps: list of steps (cubes encoded as strings)
+    """
+
+    minSteps = len(steps) - 1 # minus 1 as there are no steps to get to the solved cube
+    
+    print("Steps to solve your cube:\n")
+
+    # Reverse to show steps from instance to solved cube, rather than solved cube to instance
+    for s in reversed(steps):
+        print ("        ┌───┬───┐")
+        print ("        │ {} │ {} │".format(s[0], s[1]))
+        print ("        ├───┼───┤")
+        print ("        │ {} │ {} │".format(s[2], s[3]))
+        print ("┌───┬───┼───┼───┼───┬───┬───┬───┐")
+        print ("│ {} │ {} │ {} │ {} │ {} │ {} │ {} │ {} │".format(s[4], s[5], s[8], s[9], s[12], s[13], s[16], s[17]))
+        print ("├───┼───┼───┼───┼───┼───┼───┼───┤")
+        print ("│ {} │ {} │ {} │ {} │ {} │ {} │ {} │ {} │".format(s[6], s[7], s[10], s[11], s[14], s[15], s[18], s[19]))
+        print ("└───┴───┼───┼───┼───┴───┴───┴───┘")
+        print ("        │ {} │ {} │".format(s[20], s[21]))
+        print ("        ├───┼───┤")
+        print ("        │ {} │ {} │".format(s[22], s[23]))
+        print ("        └───┴───┘")
+        time.sleep(0.25)
+
+    # Check grammar and print
+    grammar = "are {0} steps!\n".format(minSteps)
+    if (minSteps == 1): grammar = "is 1 step!\n"
+    print("\nSOLVED!!! Minimum number of steps needed " + grammar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#█▀▄▀█ ▄▀█ █ █▄░█
+#█░▀░█ █▀█ █ █░▀█
+
+userInputCube = input("Press enter to use default cube...\nOr enter your encoded cube: ")
+defaultCube = "WYBOGWOORWGBBRYRGOGBYRYW" # min 3 steps
+
+if userInputCube:
+    print("Solving cube: {0}".format(userInputCube))
+    printSolution(solution(userInputCube))
+else:
+    print("Solving default cube: {}".format(defaultCube))
+    printSolution(solution(defaultCube))
 
 #                                              {{{{{{{{{{{{{{{{{GUIDE TO SCRAMBLING YOUR OWN CUBE}}}}}}}}}}}}}}}}}
 #                        +                               
@@ -172,7 +244,7 @@ printSolution(solution(testData))
 #                   \|,-'                                       +-------+-------+-------+-------+
 #                    +                                                  | 21 22 |
 #        {{{DESIGNED by JOHNNY MADIGAN}}}                               | 23 24 |
-#           STUDENT NUMBER: N????????                                   +-------+
+#                                                                       +-------+
 #                                               Finally, follow the order in the diagram above and note down the colour
 #                                               of the 24 stickers and you should end up with something like this: 
 #
--- src/cube_error.py
+++
@@ -1,5 +0,0 @@
-class IllegalCube(Exception):
-    """
-    Raised if user's cude is encoded badly.
-    """
-    pass
